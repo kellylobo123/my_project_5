f6a976d86ea7b27347ae4c55aad17a83a0da9908
kellylobo123 <66650798+kellylobo123@users.noreply.github.com>
2025-06-10 11:04:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles.csv') # lendo os dados
+st.title("Histograma com  tipos de carros em uma loja de carros")
 hist_button = st.button('Criar histograma')
-st.title("Análise visual de valores por tipo de carros")
+
 
 
 if hist_button: # se o botão for clicado
@@ -17,6 +18,7 @@ if hist_button: # se o botão for clicado
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
 
+st.title("Gráfico relacionando tipos de carros e seus valores")
 scatter_button = st.button('Criar Gráfico de dispersão')
 
 if scatter_button: # se o botão for clicado
